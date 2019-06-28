0befb59076e9d067b82a6789c4e3bf3b6fe8707f
Chris Foster <chris42f@gmail.com>
2019-06-28 17:34:07 +1000

    Implement AbstractString interface for FixedString
    More or less; it's not quite clear what functions are required here!

--- test/fixedstring.jl
+++ test/fixedstring.jl
@@ -5,6 +5,7 @@ using TriMRS: FixedString
     @test FixedString{5}("abcd").data == (0x61, 0x62, 0x63, 0x64, 0x00)
     @test_throws ArgumentError("String \"α\" is not ascii") FixedString{5}("α")
     @test_throws ArgumentError("String \"abcd\" does not fit into 4-1 ascii characters") FixedString{4}("abcd")
+    @test convert(FixedString{4}, FixedString{4}("abc")) === FixedString{4}("abc")
     @test convert(FixedString{4}, "abc") === FixedString{4}("abc")
     @test convert(String, FixedString{4}("abc"))::String == "abc"
 
@@ -20,4 +21,14 @@ using TriMRS: FixedString
     readbuf = IOBuffer(UInt8['a', 'b', 'c', 'd', '\0', '\0'])
     @test read(readbuf, FixedString{6}) == FixedString{6}("abcd")
     @test position(readbuf) == 6
+
+    # AbstractString interface
+    @test length(FixedString{10}("abc")) == 3
+    @test codeunit(FixedString{10}("abc")) == UInt8
+    @test ncodeunits(FixedString{10}("abc")) == 3
+    @test ncodeunits(FixedString{2}((UInt8('a'),UInt8('b')))) == 2
+
+    # Iteration
+    @test collect(FixedString{10}("abc")) == ['a', 'b', 'c']
+    @test collect(FixedString{10}("abc")) == ['a', 'b', 'c']
 end
